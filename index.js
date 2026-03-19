6f2673a196e11df8b0023add6a542e05176aa678
User15363828181728272 <defanaldrian1329@gmail.com>
2026-03-19 14:29:06 +0700

    Upload: index.js

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ app.post("/api/clone",  require("./api/clone"));
 app.get ("/api/status", require("./api/status"));
 app.post("/api/rating", require("./api/rating"));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`S-Deployment running on port ${PORT}`));
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`S-Deployment running on port ${PORT}`));
+}
+
+module.exports = app;
